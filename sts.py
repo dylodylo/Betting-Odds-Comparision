c184cee9a056e004b6c5ce2b00cd69c3f52b070f
perninio <przemekshepard@gmail.com>
2019-05-13 23:44:44 +0200

    sts- funkcja zwracająca linki dla lig z wszystkich krajów

--- sts.py
+++ sts.py
@@ -3,10 +3,37 @@ import requests
 import re
 
 #Scraping Ekstraklasa STS-website
-stsURL="https://www.sts.pl/pl/oferta/zaklady-bukmacherskie/zaklady-sportowe/?action=offer&sport=184&region=6502&league=74203&t=1556807180"
-wyniki=[]
-answer=requests.get(stsURL)
-if answer.status_code == 200:
+stsURLleuge="https://www.sts.pl/pl/oferta/zaklady-bukmacherskie/zaklady-sportowe/?action=offer&sport=184&region=6502&league=74203&t=1556807180"
+
+#zwraca tablicę linków dla wszystkich możliwych lig piłkarskich ze wszystkich krajów
+def getAllLinks(allLinkstoLeuge=[]):
+    stsURLstronaglowna="https://www.sts.pl/pl/oferta/zaklady-bukmacherskie/zaklady-sportowe/?action=offer&sport=184&t=1557777401" 
+    leugesLinks=[]
+    allLinkstoLeuge=[]
+    answer=requests.get(stsURLstronaglowna)
+    if answer.status_code==200:
+     soup=BeautifulSoup(answer.content,'html.parser')
+     druzyny=soup.find(id="sport_184")
+     linki=druzyny.find_all(href=True)  
+     for link in linki:           
+        leugesLinks.append(link.get('href'))        
+        for linklig in leugesLinks:
+         answer2=requests.get(linklig)
+         if answer2.status_code==200:
+            soup2=BeautifulSoup(answer2.content,'html.parser')
+            names = re.findall(r'league_[0,1,2,3,4,5,6,7,8,9,0]*',str(soup2) )
+            for name in names:
+                allLinkstoLeuge.append(soup2.find(id=str(name)).a.get('href'))
+                #print(soup2.find(id=str(name)).a.get('href'))
+     return allLinkstoLeuge
+    else:
+        print('blad')
+
+#scrapowanie kursów ze strony
+def scrapMatches(stsURLleuge,wyniki=[]):
+ wyniki=[]
+ answer=requests.get(stsURLleuge)
+ if answer.status_code == 200:
     soup=BeautifulSoup(answer.content,'html.parser')
     mecze=soup.find(id="offerTables")
     dzien=mecze.find_all(class_="col3")
@@ -46,5 +73,10 @@ if answer.status_code == 200:
                 print("Drużyna 2 kurs: "+druzyna2[1])
                 
                 wyniki.append({'druzyna1':druzyna1Nazwa,'kursdruzyna1':druzyna1Kurs,'remis':KursRemis,'druzyna2':druzyna2Nazwa,'kursdruzyna2':druzyna2Kurs})
-else:
-    print("Błąd: "+answer.status_code)
+                return wyniki
+ else:
+     print("Błąd: "+answer.status_code)
+
+
+
+

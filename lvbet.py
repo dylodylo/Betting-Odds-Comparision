5288c2dc21404716517def094321ef8694469449
Artur Dylewski <artur.dylewski.27@gmail.com>
2019-05-15 01:28:34 +0200

    Scrapowanie poszczególnych kursów
    Do poprawienia:
    - rozbicie try na 1X2 i 1X X2 i 12 aby nie wywalało błędów gdy jest tylko 1X2
    - poprawienie wyłuskiwania nazw drużyn

--- lvbet.py
+++ lvbet.py
@@ -16,16 +16,16 @@ print(urlpage)
 driver = webdriver.Firefox()
 driver.get(urlpage)
 time.sleep(3)
-football_countries = []
-counter = 0
+#
 # parse html
-form = driver.find_element_by_xpath('//*[@class="col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted"]')
 page_content = BeautifulSoup(driver.page_source, "html.parser")
 sports_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
 
-
+football_countries = []
+counter = 0
 for a in sports_container:
     link_text = a.attrs['href']
+    print('pierwsza petla')
     if link_text != '/pl/zaklady-bukmacherskie':
         print('Link: https://lvbet.pl' + link_text)
         a_league_site = 'https://lvbet.pl' + link_text
@@ -36,9 +36,10 @@ for a in sports_container:
         counter =+1
 
 football_leagues = []
-counter = 0
 countries = football_countries
 for x in countries:
+    counter = 0
+    print('druga petla')
     print(x.league_site)
     driver.get(str(x.league_site))
     page_content = BeautifulSoup(driver.page_source, "html.parser")
@@ -46,83 +47,39 @@ for x in countries:
     for y in leagues_container:
         link_text = y.attrs['href']
         if link_text != '/pl/zaklady-bukmacherskie':
-            print(x.league_site + link_text)
-            a_league_site = x.league_site + link_text
+            print('https://lvbet.pl' + link_text)
+            a_league_site = 'https://lvbet.pl' + link_text
             a_league_id = counter
             a_league_name = "league " + str(counter)
             country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
             football_leagues.append(country)
-            counter = +1
+            counter = counter +1
 
-# import sys
-# from PyQt5.QtWebEngineWidgets import QWebEnginePage
-# from PyQt5.QtWidgets import QApplication
-# from PyQt5.QtCore import QUrl
-# page_link = None
-#
-# class Page(QWebEnginePage):
-#     def __init__(self, url):
-#         self.app = QApplication(sys.argv)
-#         QWebEnginePage.__init__(self)
-#         self.html = ''
-#         self.loadFinished.connect(self._on_load_finished)
-#         self.load(QUrl(url))
-#         self.app.exec_()
-#
-#     def _on_load_finished(self):
-#         self.html = self.toHtml(self.Callable)
-#         print('Load finished')
-#
-#     def Callable(self, html_str):
-#         self.html = html_str
-#         self.app.quit()
-#
-#
-# #ładuje WWSZYSTKIE ligi do kontenera
-# def load_countries():
-#     football_countries = []
-#     counter = 0
-#     global page_link
-#     page_link = Page('https://lvbet.pl/pl/zaklady-bukmacherskie/5/pilka-nozna')
-#     # fetch the content from url
-#     #page_response = requests.get(page_link, timeout=5)
-#     # parse html
-#     page_content = BeautifulSoup(page_link.html, "html.parser")
-#     sports_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
-#
-#     for a in sports_container:
-#         link_text = a.attrs['href']
-#         if link_text != '/pl/zaklady-bukmacherskie':
-#             print('Link: https://lvbet.pl' + link_text)
-#             a_league_site = 'https://lvbet.pl' + link_text
-#             a_league_id = counter
-#             a_league_name = "league " + str(counter)
-#             country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
-#             football_countries.append(country)
-#             counter =+1
-#     return football_countries
-#
-# def load_leagues():
-#     football_leagues = []
-#     counter = 0
-#     countries = load_countries()
-#     for x in countries:
-#         print(x.league_site)
-#         global page_link
-#         page_link = Page(str(x.league_site))
-#         page_content = BeautifulSoup(page_link.html, "html.parser")
-#         leagues_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
-#         for y in leagues_container:
-#             link_text = y.attrs['href']
-#             if link_text != '/pl/zaklady-bukmacherskie':
-#                 print(x.league_site + link_text)
-#                 a_league_site = x.league_site + link_text
-#                 a_league_id = counter
-#                 a_league_name = "league " + str(counter)
-#                 country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
-#                 football_leagues.append(country)
-#                 counter = +1
-#     return football_leagues
-#
-#
-# load_leagues()
+
+for x in football_leagues:
+    link = x.league_site
+    print(link)
+    driver.get(str(link))
+    time.sleep(3)
+    slash = link.rfind('/')
+    text = link[slash+1:]
+    dash = text.find('-')
+    text_array = list(text)
+    text_array[dash] = ' '
+    newtext = "".join(text_array)
+
+    page_content = BeautifulSoup(driver.page_source, "html.parser")
+    matches_container = page_content('div', class_='row lv-table-entry')
+    for y in matches_container:
+        teams = y('div', class_='col-d-5 col-t-12 teams')
+        teams3 = y.find_all('p')
+        odds = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-12 ng-star-inserted")
+        odds2 = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-hidden ng-star-inserted")
+        foramoment = teams[0].text.lower()
+        slice = foramoment.find(newtext)
+        slice = slice+len(newtext)
+        try:
+            oddsarray = odds[0].text.split(' ')
+            print(teams[0].text[slice+2:] + oddsarray[1] + ' ' + oddsarray[3] + ' ' + oddsarray[5] + ' ' + odds2[0].text)
+        except:
+            print("Problem z listami odds")

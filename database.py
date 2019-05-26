1eb7b75fe58c7c8bdc13a937f7958f23a978584f
Artur Dylewski <artur.dylewski.27@gmail.com>
2019-05-26 15:27:17 +0200

    Lvbet + bd
    - Praktycznie skończone scrapowanie Lvbeta (zostało dodawanie lig, które pobierają się już przy pobieraniu krajów)
    - Dodanie w database.py funkcji dodających i obsługujących tabele dla STS i LVBet

--- database.py
+++ database.py
@@ -5,6 +5,18 @@ conn = sqlite3.connect('bazadanych.db')
 
 c = conn.cursor()
 
+def delete():
+    c.execute("DROP TABLE IF EXISTS Lvbet_matches")
+    c.execute("DROP TABLE IF EXISTS Lvbet_match_odds")
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS Lvbet_match_odds(id INT PRIMARY KEY, jed FLOAT, X FLOAT, dwa FLOAT, jX FLOAT, Xd FLOAT, jd FLOAT, league_id INT, FOREIGN KEY(league_id) REFERENCES Fortuna_leagues(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS Lvbet_matches(id INT PRIMARY KEY, t1 STRING, t2 STRING)')
+def get_leagues():
+    query = "SELECT * FROM Lvbet_leagues"
+    c.execute(query)
+    result = c.fetchall()
+    return result
+
 def create_table(): #tworzenie wszystkich tabel
     c.execute('CREATE TABLE IF NOT EXISTS Fortuna_leagues(id INT PRIMARY KEY, site STRING, name STRING)')
     c.execute('CREATE TABLE IF NOT EXISTS Fortuna_match_odds(id INT PRIMARY KEY, jed FLOAT, X FLOAT, dwa FLOAT, jX FLOAT, Xd FLOAT, jd FLOAT, league_id INT, FOREIGN KEY(league_id) REFERENCES Fortuna_leagues(id))')
@@ -12,6 +24,12 @@ def create_table(): #tworzenie wszystkich tabel
     c.execute('CREATE TABLE IF NOT EXISTS Forbet_leagues(id INT PRIMARY KEY, site STRING, name STRING)')
     c.execute('CREATE TABLE IF NOT EXISTS Forbet_match_odds(id INT PRIMARY KEY, jed FLOAT, X FLOAT, dwa FLOAT, jX FLOAT, Xd FLOAT, jd FLOAT, league_id INT, FOREIGN KEY(league_id) REFERENCES Forbet_leagues(id))')
     c.execute('CREATE TABLE IF NOT EXISTS Forbet_matches(id INT PRIMARY KEY, t1 STRING, t2 STRING)')
+    c.execute('CREATE TABLE IF NOT EXISTS Lvbet_leagues(id INT PRIMARY KEY, site STRING, name STRING)')
+    c.execute('CREATE TABLE IF NOT EXISTS Lvbet_match_odds(id INT PRIMARY KEY, jed FLOAT, X FLOAT, dwa FLOAT, jX FLOAT, Xd FLOAT, jd FLOAT, league_id INT, FOREIGN KEY(league_id) REFERENCES Fortuna_leagues(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS Lvbet_matches(id INT PRIMARY KEY, t1 STRING, t2 STRING)')
+    c.execute('CREATE TABLE IF NOT EXISTS Sts_leagues(id INT PRIMARY KEY, site STRING, name STRING)')
+    c.execute('CREATE TABLE IF NOT EXISTS Sts_match_odds(id INT PRIMARY KEY, jed FLOAT, X FLOAT, dwa FLOAT, jX FLOAT, Xd FLOAT, jd FLOAT, league_id INT, FOREIGN KEY(league_id) REFERENCES Fortuna_leagues(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS Sts_matches(id INT PRIMARY KEY, t1 STRING, t2 STRING)')
 
 def data_entry():
     c.execute("INSERT INTO match VALUES(1, 2.5, 2.4, 3.2, 1.4, 1.3, 1.2)")
@@ -61,6 +79,55 @@ def Forbet_match_entry(id, t1, t2):
         pass
     conn.commit()
 
+def Lvbet_leagues_entry(id, site, name):
+    try:
+        c.execute("INSERT INTO Lvbet_leagues VALUES (?, ?, ?)", (id, site, name))
+    except sqlite3.IntegrityError as ie:
+        print("blad z dodaniem lig")
+    conn.commit()
+
+def Lvbet_odds_data_entry(id, odd1, oddx, odd2, odd1x, oddx2, odd12, league_id):
+    try:
+        c.execute("INSERT INTO Lvbet_match_odds VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (id, odd1, oddx, odd2, odd1x, oddx2, odd12, league_id))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
+
+def Lvbet_odds_data_entry_not_full(id, odd1, oddx, odd2, league_id):
+    try:
+        c.execute("INSERT INTO Lvbet_match_odds VALUES (?, ?, ?, ?, 0, 0, 0, ?)", (id, odd1, oddx, odd2, league_id))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
+
+def Lvbet_match_entry(id, t1, t2):
+    try:
+        c.execute('INSERT INTO Lvbet_matches VALUES (?, ?, ?)', (id, t1, t2))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
+
+def Sts_leagues_entry(id, site, name):
+    try:
+        c.execute("INSERT INTO Sts_leagues VALUES (?, ?, ?)", (id, site, name))
+    except sqlite3.IntegrityError as ie:
+        print("blad z dodaniem lig")
+    conn.commit()
+
+def Sts_odds_data_entry(id, odd1, oddx, odd2, league_id):
+    try:
+        c.execute("INSERT INTO Sts_match_odds VALUES (?, ?, ?, ?, 0, 0, 0, ?)", (id, odd1, oddx, odd2, league_id))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
+
+def Sts_match_entry(id, t1, t2):
+    try:
+        c.execute('INSERT INTO Sts_matches VALUES (?, ?, ?)', (id, t1, t2))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
+
 def delete_table(): #usuwanie wszystkich tabel
     c.execute("DROP TABLE IF EXISTS Fortuna_match_odds")
     c.execute("DROP TABLE IF EXISTS Forbet_match_odds")
@@ -68,6 +135,12 @@ def delete_table(): #usuwanie wszystkich tabel
     c.execute("DROP TABLE IF EXISTS Forbet_matches")
     c.execute("DROP TABLE IF EXISTS Fortuna_leagues")
     c.execute("DROP TABLE IF EXISTS Forbet_leagues")
+    c.execute("DROP TABLE IF EXISTS Lvbet_matches")
+    c.execute("DROP TABLE IF EXISTS Lvbet_match_odds")
+    c.execute("DROP TABLE IF EXISTS Lvbet_leagues")
+    c.execute("DROP TABLE IF EXISTS Sts_matches")
+    c.execute("DROP TABLE IF EXISTS Sts_match_odds")
+    c.execute("DROP TABLE IF EXISTS Sts_leagues")
 
 #funkcja do umieszczania zespołów w tabeli
 def insert_teams():
--- lvbet.py
+++ lvbet.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import Fortuna
 import time
-import requests
 import unidecode
-
+import database
+database.delete_table()
+database.create_table()
 # specify the url
 urlpage = 'https://lvbet.pl/pl/zaklady-bukmacherskie/5/pilka-nozna'
 print(urlpage)
@@ -15,77 +15,126 @@ driver.get(urlpage)
 time.sleep(3)
 
 
-#parse html
-# page_content = BeautifulSoup(driver.page_source, "html.parser")
-# sports_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
-#
-# football_countries = []
-# counter = 0
-# for a in sports_container:
-#     link_text = a.attrs['href']
-#     if link_text != '/pl/zaklady-bukmacherskie':
-#         print('Link: https://lvbet.pl' + link_text)
-#         a_league_site = 'https://lvbet.pl' + link_text
-#         a_league_id = counter
-#         a_league_name = "league " + str(counter)
-#         country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
-#         football_countries.append(country)
-#         counter =+1
-#
-# football_leagues = []
-# countries = football_countries
-# for x in countries:
-#     counter = 0
-#     print(x.league_site)
-#     driver.get(str(x.league_site))
-#     page_content = BeautifulSoup(driver.page_source, "html.parser")
-#     leagues_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
-#     for y in leagues_container:
-#         link_text = y.attrs['href']
-#         if link_text != '/pl/zaklady-bukmacherskie':
-#             print('https://lvbet.pl' + link_text)
-#             a_league_site = 'https://lvbet.pl' + link_text
-#             a_league_id = counter
-#             a_league_name = "league " + str(counter)
-#             country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
-#             football_leagues.append(country)
-#             counter = counter +1
-#
-#
-# for x in football_leagues:
-link = 'https://lvbet.pl/pl/zaklady-bukmacherskie/5/563/564/pilka-nozna/wlochy/2.-liga'
-print(link)
-driver.get(str(link))
-time.sleep(3)
-slash = link.rfind('/')
-text = link[slash+1:]
-p = text.rfind('%')
-text2 = text[:p]
-text_array = list(text2)
-if '-' in text_array:
-    isdash = 1
-else:
-    isdash = 0
-while isdash == 1:
-    index = text_array.index('-')
-    text_array[index] = ' '
+# # #parse html
+page_content = BeautifulSoup(driver.page_source, "html.parser")
+sports_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
+
+countries = []
+for a in sports_container:
+    link_text = a.attrs['href']
+    if link_text != '/pl/zaklady-bukmacherskie':
+        print('Link: https://lvbet.pl' + link_text)
+        country_site = 'https://lvbet.pl' + link_text
+        countries.append(country_site)
+
+football_leagues = []
+counter = 0
+for x in countries:
+    print(x)
+    driver.get(x)
+    page_content = BeautifulSoup(driver.page_source, "html.parser")
+    leagues_container = page_content('a', class_='col-d-3 col-mt-4 col-st-6 col-sm-12 ng-star-inserted')
+    for y in leagues_container:
+        link_text = y.attrs['href']
+        if link_text != '/pl/zaklady-bukmacherskie':
+            print('https://lvbet.pl' + link_text)
+            a_league_site = 'https://lvbet.pl' + link_text
+            a_league_id = counter
+            a_league_name = "league " + str(counter)
+            country = Fortuna.League_Fortuna(a_league_id, a_league_name, a_league_site)
+            football_leagues.append(country)
+            database.Lvbet_leagues_entry(a_league_id, a_league_site, a_league_name)
+            counter = counter +1
+
+football_leagues = database.get_leagues()
+counter = 0
+
+for x in football_leagues:
+    link = x[1]
+    print(link)
+    driver.get(str(link))
+    time.sleep(3)
+    slash = link.rfind('/')
+    text = link[slash+1:]
+    p = text.rfind('%')
+    dash = text.rfind('-')
+    if (p>0):
+        text2 = text[:p]
+    else:
+        text2 = text
+    if text2.endswith('-'):
+        text2 = text2[:-1]
+        dash = text2.rfind('-')
+    dashtext = text2[dash+1:]
+    newdashtext = ' ' + dashtext + ' '
+    text_array = list(text2)
     if '-' in text_array:
         isdash = 1
     else:
         isdash = 0
-newtext = "".join(text_array)
+    while isdash == 1:
+        index = text_array.index('-')
+        text_array[index] = ' '
+        if '-' in text_array:
+            isdash = 1
+        else:
+            isdash = 0
+    newtext = "".join(text_array)
+    page_content = BeautifulSoup(driver.page_source, "html.parser")
+    matches_container = page_content('div', class_='row lv-table-entry')
+    for y in matches_container:
+        oddsarray = []
+        oddsarray2 = []
+        teams = y('div', class_='col-d-5 col-t-12 teams')
+        odds = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-12 ng-star-inserted")
+        odds2 = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-hidden ng-star-inserted")
+        foramoment = unidecode.unidecode(teams[0].text.lower())
+        if foramoment.find(newdashtext) > 0:
+            slice = foramoment.find(newdashtext)
+            slice = slice+len(dashtext)
+        else:
+            slice = foramoment.find(dashtext)
+            slice = slice + len(dashtext)
 
-page_content = BeautifulSoup(driver.page_source, "html.parser")
-matches_container = page_content('div', class_='row lv-table-entry')
-for y in matches_container:
-    teams = y('div', class_='col-d-5 col-t-12 teams')
-    odds = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-12 ng-star-inserted")
-    odds2 = y('div', class_="col-d-2 col-md-3 col-sd-2 col-t-3 col-st-6 col-sm-hidden ng-star-inserted")
-    foramoment = unidecode.unidecode(teams[0].text.lower())
-    slice = foramoment.find(newtext)
-    slice = slice+len(newtext)
-    try:
-        oddsarray = odds[0].text.split(' ')
-        print(teams[0].text[slice+2:] + oddsarray[1] + ' ' + oddsarray[3] + ' ' + oddsarray[5] + ' ' + odds2[0].text)
-    except:
-        print("Problem z listami odds")
+
+        #wyłuskanie zespołów
+        if teams[0].text[slice+2:].count(' - ') > 1:
+            index = teams[0].text[slice + 2:].find(' - ')
+            slice = slice+index+1
+            index = teams[0].text[slice:].find(' ')
+            slice = slice+index
+        teams = teams[0].text[slice+2:]
+        dash = teams.find('-')
+        if (teams[:dash].lstrip() != teams[dash+2:].rstrip()): #wykluczenie nazw zakladow na zwyciezcow
+            t1 = teams[:dash-2].lstrip()
+            t2 = teams[dash+2:].rstrip()
+            database.Lvbet_match_entry(counter, t1, t2)
+            print(t1 + ' - ' + t2)
+            try:
+                oddsarray = odds[0].text.split(' ')
+            except:
+                try:
+                    oddsarray = y('div', class_='col-d-6 col-t-9 col-st-12 ng-star-inserted')[0].text.split(' ')
+                except:
+                    try:
+                        oddsarray = y('div', class_='col-d-3 col-md-3 col-t-5 col-st-6 ng-star-inserted')[0].text.split(' ')
+                    except:
+                        print('brak array')
+            try:
+                oddsarray2 = odds2[0].text.split(' ')
+            except:
+                print('brak array2')
+
+            if oddsarray2 and oddsarray2 != ['']:
+                try:
+                    print(oddsarray[1] + ' ' + oddsarray[3] + ' ' + oddsarray[5] + ' ' + oddsarray2[1] + ' ' + oddsarray2[3] + ' ' + oddsarray2[5])
+                    database.Lvbet_odds_data_entry(counter, oddsarray[1], oddsarray[3], oddsarray[5], oddsarray2[1], oddsarray2[3], oddsarray2[5], x[0])
+                except:
+                    print("Problem z listami odds")
+            else:
+                try:
+                    print(oddsarray[1] + ' ' + oddsarray[3] + ' ' + oddsarray[5])
+                    database.Lvbet_odds_data_entry_not_full(counter, oddsarray[1], oddsarray[3], oddsarray[5], x[0])
+                except:
+                    print("Problem z listami odds bez odds2")
+            counter = counter + 1

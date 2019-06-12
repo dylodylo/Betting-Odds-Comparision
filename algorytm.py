3fc4a22cc9ba3d809f32d572f1aa1ca7ddfac979
IvanNovgorodtsev <ivan.novgorodtsev@gmail.com>
2019-06-12 12:38:48 +0200

    Update algorytm.py
    Algorytm porównujący

--- algorytm.py
+++ algorytm.py
@@ -1,3 +1,4 @@
+import difflib
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
@@ -20,15 +21,18 @@ def forbet_teams():
     elements = driver.find_elements_by_xpath("//div[@class='leftMenu__item leftMenu__item--nested1' and @data-menu]/div[@class='leftMenu__subheader']")
     def get_leagues():
         urls = list()
-        #count = 0
+        #tests
+        count = 0
+        #
         for e in elements:
             e.click()
             html = driver.page_source
-            sleep(3)
             soup = BeautifulSoup(html, 'html.parser')
             urls.append(str(soup.findAll('div', {"class": "leftMenu__content leftMenu__content--hidden opened"})))
-            #if(count==3): break
-            #else: count=count+1
+            # tests
+            if(count==10): break
+            else: count=count+1
+            #
         return urls
 
     urls = get_leagues()
@@ -36,13 +40,17 @@ def forbet_teams():
     for url in urls:
         leagues.append(re.findall('href=[\'"]?([^\'" >]+)', url))
 
+    teams = list()
     for x in itertools.chain.from_iterable(leagues):
         page = requests.get(x)
         soup = BeautifulSoup(page.content, 'html.parser')
         table_body = soup.find_all('body')
         name = str(re.findall(r'<a[\s]+[^>]*?href[\s]?=[\s\"\']*(.*?)[\"\']*.*?>([^<]+|.*?)?<\/a>', str(table_body)))
         teams_forbet = re.findall("'\w+[\s]\w+'", name, re.UNICODE)
-        print(str(x)+": "+str(teams_forbet))
+        #print(str(x)+": "+str(teams_forbet))
+        for team in teams_forbet:
+             teams.append(team)
+    return teams
 
 def fortuna_teams():
     league = list()
@@ -65,8 +73,9 @@ def fortuna_teams():
         #   print (leagues_name.text)
         for y in leagues:
             league.append(y.attrs['href'])
-
+    # tests
     count = 0
+    #
     for link in league:
         key = link
         page = requests.get('https://s5.sir.sportradar.com' + link)
@@ -79,26 +88,78 @@ def fortuna_teams():
                 teams[key].append(name.text)
             else:
                 teams[link].append(name.text)
-        if (count == 3):
-            return teams
+        # tests
+        if (count == 10):
+           return teams
+
         else:
             count = count + 1
     return teams
 
+scrapped_fortuna = list()
+for key, values in fortuna_teams().items():
+    for value in values:
+        scrapped_fortuna.append(value)
 
-
-def teams_database():
-    c.execute('CREATE TABLE IF NOT EXISTS Forbet_teams(id INT PRIMARY KEY, team_name STRING)')
-    c.execute('CREATE TABLE IF NOT EXISTS Fortuna_teams(id INT PRIMARY KEY, team_name STRING)')
-
+def forbet_insert():
+    c.execute('CREATE TABLE IF NOT EXISTS Forbet_teams(forbet_id INTEGER PRIMARY KEY,team_name STRING)')
+    insert_teams = list()
+    insert_teams = forbet_teams()
     try:
-        for team in fortuna_teams():
-            c.execute("INSERT INTO Fortuna_teams VALUES (?, ?)", (id, team))
+        without_duplicates = list(dict.fromkeys(insert_teams))
+        for team in without_duplicates:
+            c.execute('INSERT INTO Forbet_teams VALUES (NULL, ?)', (team,))
     except sqlite3.IntegrityError as ie:
         pass
     conn.commit()
 
-def relationship_database():
-    c.execute('CREATE TABLE IF NOT EXISTS Relationship_table (id INT PRIMARY KEY, team_name STRING, id_fortuna STRING, id_forbet STRING)')
+def fortuna_insert():
+    c.execute('CREATE TABLE IF NOT EXISTS Fortuna_teams(fortuna_id INTEGER PRIMARY KEY, team_name STRING)')
+    insert_teams = list()
+    insert_teams = scrapped_fortuna
+    try:
+        without_duplicates = list(dict.fromkeys(insert_teams))
+        for team in without_duplicates:
+            c.execute('INSERT INTO Fortuna_teams VALUES (NULL, ?)', ("'" +team+ "'",))
+    except sqlite3.IntegrityError as ie:
+        pass
+    conn.commit()
 
-print (fortuna_teams())
+
+def relationship_database():
+    c.execute("select * from Fortuna_teams")
+    records = c.fetchall()
+    fortuna = list()
+    for row in records:
+        fortuna.append(row[1])
+
+    c.execute("select * from Forbet_teams")
+    records = c.fetchall()
+    forbet= list()
+    for row in records:
+        forbet.append(row[1])
+
+    c.execute('CREATE TABLE IF NOT EXISTS Relationship_table (team_name STRING, id_fortuna STRING, id_forbet STRING)')
+    # test
+    count = 0
+    #
+    try:
+         for team in forbet:
+             if (count==1000): break
+             else: count=count+1
+             if(team in fortuna):
+                 c.execute("INSERT INTO Relationship_table VALUES (?, ?, ?)", (team, fortuna.index(team), forbet.index(team) ))
+                 conn.commit()
+             else:
+                 name = difflib.get_close_matches(team,fortuna,cutoff=0.95)
+                 if name:
+                    c.execute("INSERT INTO Relationship_table VALUES (?, ?, ?)", (team, fortuna.index(name[0]), forbet.index(team) ))
+                    conn.commit()
+                 else: continue
+    except sqlite3.IntegrityError as ie:
+        pass
+
+
+#fortuna_insert()
+#forbet_insert()
+relationship_database()

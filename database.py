2a8c098e6f82853d5ce63be4d3bf7723354e8d70
Artur Dylewski <adylewski@DOMITTI.local>
2019-07-19 13:53:57 +0200

    Bugfixes
    - Poprawka funkcji insert_teams
    - Poprawka funkcji matchteams

--- database.py
+++ database.py
@@ -191,26 +191,26 @@ def insert_teams(bookie):
     data = c.fetchall()
     i = 0
     for row in data:
-        row = str(row)[2:].rstrip("\',)").rstrip('\"').replace("'","")
-        row = row.replace("'", "")
-        print(row)
-        c.execute("SELECT " + bookie + "_name FROM " + bookie + "_teams WHERE " + bookie + "_name = '"+row+"'")
+        team = row[0]
+        print(team)
+        c.execute("SELECT " + bookie + "_name FROM " + bookie + "_teams WHERE " + bookie + '_name = "'+team+'"')
         pom = c.fetchall()
         if  len(pom) > 0:
             pass
         else:
-            c.execute('INSERT INTO ' + bookie + '_teams (id, ' + bookie + '_name) VALUES (?,?)', (i, row))
+            c.execute('INSERT INTO ' + bookie + '_teams (id, ' + bookie + '_name) VALUES (?,?)', (i, team))
             i = i+1
     c.execute("SELECT t2 FROM " + bookie + "_matches")
     data = c.fetchall()
     for row in data:
-        row = str(row)[2:].rstrip("\',)").replace("'","")
-        c.execute("SELECT " + bookie + "_name FROM " + bookie + "_teams WHERE " + bookie + "_name = '"+row+"'")
+        team = row[0]
+        print(team)
+        c.execute("SELECT " + bookie + "_name FROM " + bookie + "_teams WHERE " + bookie + '_name = "'+team+'"')
         pom = c.fetchall()
         if len(pom) > 0:
             pass
         else:
-            c.execute('INSERT INTO ' + bookie + '_teams (id, ' + bookie + '_name) VALUES (?,?)', (i, row))
+            c.execute('INSERT INTO ' + bookie + '_teams (id, ' + bookie + '_name) VALUES (?,?)', (i, team))
             i = i+1
     print(data)
     conn.commit()
@@ -222,232 +222,34 @@ def insert_all_teams():
     insert_teams("Milenium")
 
 
-def match_leagues():
+def create_matchedleagues_table():
+    c.execute('CREATE TABLE IF NOT EXISTS matched_leagues(idFortuna INT, siteFortuna STRING, '
+              'idForbet INT, siteForbet STRING, FOREIGN KEY (idFortuna) REFERENCES Fortuna_leagues(id), FOREIGN KEY '
+              '(idForbet) REFERENCES Forbet_leagues(id))')
 
-    c.execute("SELECT * FROM Fortuna_leagues")
-    leagues_Fortuna_data = c.fetchall()
-    c.execute("SELECT * FROM Forbet_leagues")
-    leagues_Forbet_data = c.fetchall()
-    c.execute("CREATE TABLE IF NOT EXISTS Matched_Leagues (forbet_id INT, fortuna_id INT)")
-    MatchedLeaguesFA = [0] * leagues_Fortuna_data.__sizeof__()
-    MatchedLeaguesFT = [0] * leagues_Forbet_data.__sizeof__()
-    findLeague = None
-    for row in leagues_Fortuna_data:
-        if MatchedLeaguesFA[row[0]] == False:
-            c.execute("SELECT t1, t2 FROM Fortuna_matches AS fm INNER JOIN Fortuna_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Fortuna_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = " + str(row[0]))
-            Fortuna_matches_data = c.fetchall()
-            for fone_match in Fortuna_matches_data:
-                for roow in leagues_Forbet_data:
-                    findLeague = False
-                    #if MatchedLeaguesFT[roow[0]] == False:
-                    c.execute("SELECT t1, t2 FROM Forbet_matches AS fm INNER JOIN Forbet_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Forbet_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = " + str(roow[0]))
-                    Forbet_matches_data = c.fetchall()
-                    for one_match in Forbet_matches_data:
-                        if jellyfish.jaro_distance(str(fone_match[0]), str(one_match[0])) > 0.8 or jellyfish.jaro_distance(str(fone_match[1]), str(one_match[1])) > 0.8:
-                            print(Forbet_matches_data)
-                            print(Fortuna_matches_data)
-                            s = input('match? y/n -> ')
-                            if s == 'y':
-                                findLeague = True
-                                MatchedLeaguesFT[roow[0]] = True
-                                MatchedLeaguesFA[row[0]] = True
-                                c.execute("INSERT INTO Matched_Leagues VALUES (?, ?)", (roow[0], row[0]
-                                                                                       ))
-                                conn.commit()
-                                break
-                    if findLeague == True:
-                        break
-                if findLeague == True:
-                    break
 
-def teams_from_league():
-    c.execute("SELECT * FROM Matched_Leagues")
-    leagues_data = c.fetchall()
-    i = 0
-    isFind = False
-    listaostaczena = []
-    for row in leagues_data:
-        forbetid = row[0]
-        fortunaid = row[1]
-        forbettoDelete = []
-        fortunatoDelete = []
-        c.execute(
-            "SELECT t1, t2 FROM Fortuna_matches AS fm INNER JOIN Fortuna_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Fortuna_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(fortunaid)+"'")
-        fortuna_teams = c.fetchall()
-        print(len(fortuna_teams))
-        c.execute(
-            "SELECT t1, t2 FROM Forbet_matches AS fm INNER JOIN Forbet_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Forbet_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(forbetid)+"'")
-        forbet_teams = c.fetchall()
-        for rowinforbet in forbet_teams:
-                for rowinfortuna in fortuna_teams:
-                    #print (str(rowinforbet[0]) + " " + str(jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]))+" "+ str(rowinfortuna[0]) + " " + str(rowinforbet[1]) + " " + str(jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1])) + " " + str(rowinfortuna[1]))
-                    if jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]) > 0.7 and jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1]) > 0.7:
-                        print(rowinforbet + rowinfortuna)
-                        i +=1
-                        forbettoDelete.append(rowinforbet)
-                        fortunatoDelete.append(rowinfortuna)
-                        listaostaczena.append(rowinforbet)
-                        listaostaczena.append(rowinfortuna)
-                        isFind = True
-                        break
-                if isFind == False:
-                    print(rowinforbet)
-                else:
-                    isFind = False
+def create_matchedteams_table():
+    c.execute('CREATE TABLE IF NOT EXISTS matched_teams(id INTEGER PRIMARY KEY, idFortuna INT, idForbet INT, '
+              'nameFortuna STRING, nameForbet STRING, FOREIGN KEY (idFortuna) REFERENCES Fortuna_teams(id), '
+              'FOREIGN KEY (idForbet) REFERENCES Forbet_teams(id))')
 
-        if len(forbettoDelete) > 0:
-            for rowinrow in forbettoDelete:
-                forbet_teams.remove(rowinrow)
-        if len(fortunatoDelete) > 0:
-            for rowinrow2 in fortunatoDelete:
-                if len(fortuna_teams) > 0:
-                    try:
-                        fortuna_teams.remove(rowinrow2)
-                    except ValueError:
-                        print("blad z meczem " + str(rowinrow2))
-        print(forbet_teams)
-        print(fortuna_teams)
-    print(i)
-    for row in leagues_data:
-        forbetid = row[0]
-        fortunaid = row[1]
-        forbettoDelete = []
-        fortunatoDelete = []
-        c.execute(
-            "SELECT t1, t2 FROM Fortuna_matches AS fm INNER JOIN Fortuna_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Fortuna_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(fortunaid)+"'")
-        fortuna_teams = c.fetchall()
-        print(len(fortuna_teams))
-        c.execute(
-            "SELECT t1, t2 FROM Forbet_matches AS fm INNER JOIN Forbet_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Forbet_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(forbetid)+"'")
-        forbet_teams = c.fetchall()
-        for rowinforbet in forbet_teams:
-                for rowinfortuna in fortuna_teams:
-                    if rowinfortuna in listaostaczena:
-                        isFind == True
-                    else:
-                        #print (str(rowinforbet[0]) + " " + str(jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]))+" "+ str(rowinfortuna[0]) + " " + str(rowinforbet[1]) + " " + str(jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1])) + " " + str(rowinfortuna[1]))
-                        if jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]) > 0.6 and jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1]) > 0.6:
-                            print(rowinforbet + rowinfortuna)
-                            i +=1
-                            forbettoDelete.append(rowinforbet)
-                            fortunatoDelete.append(rowinfortuna)
-                            listaostaczena.append(rowinforbet)
-                            listaostaczena.append(rowinfortuna)
-                            isFind = True
-                            break
-                if isFind == False:
-                    print(rowinforbet)
-                else:
-                    isFind = False
-        listaostaczena.append(forbettoDelete)
-        listaostaczena.append(fortunatoDelete)
-        if len(forbettoDelete) > 0:
-            for rowinrow in forbettoDelete:
-                forbet_teams.remove(rowinrow)
-        if len(fortunatoDelete) > 0:
-            for rowinrow2 in fortunatoDelete:
-                if len(fortuna_teams) > 0:
-                    try:
-                        fortuna_teams.remove(rowinrow2)
-                    except ValueError:
-                        print("blad z meczem " + str(rowinrow2))
-        print(forbet_teams)
-        print(fortuna_teams)
-    print(i)
-    for row in leagues_data:
-        forbetid = row[0]
-        fortunaid = row[1]
-        forbettoDelete = []
-        fortunatoDelete = []
-        c.execute(
-            "SELECT t1, t2 FROM Fortuna_matches AS fm INNER JOIN Fortuna_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Fortuna_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(fortunaid)+"'")
-        fortuna_teams = c.fetchall()
-        print(len(fortuna_teams))
-        c.execute(
-            "SELECT t1, t2 FROM Forbet_matches AS fm INNER JOIN Forbet_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Forbet_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(forbetid)+"'")
-        forbet_teams = c.fetchall()
-        for rowinforbet in forbet_teams:
-                for rowinfortuna in fortuna_teams:
-                    if rowinfortuna in listaostaczena:
-                        isFind == True
-                    else:
-                        #print (str(rowinforbet[0]) + " " + str(jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]))+" "+ str(rowinfortuna[0]) + " " + str(rowinforbet[1]) + " " + str(jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1])) + " " + str(rowinfortuna[1]))
-                        if jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]) > 0.5 and jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1]) > 0.5:
-                            print(rowinforbet + rowinfortuna)
-                            i +=1
-                            forbettoDelete.append(rowinforbet)
-                            fortunatoDelete.append(rowinfortuna)
-                            listaostaczena.append(rowinforbet)
-                            listaostaczena.append(rowinfortuna)
-                            isFind = True
-                            break
-                if isFind == False:
-                    print(rowinforbet)
-                else:
-                    isFind = False
-        listaostaczena.append(forbettoDelete)
-        listaostaczena.append(fortunatoDelete)
-        if len(forbettoDelete) > 0:
-            for rowinrow in forbettoDelete:
-                forbet_teams.remove(rowinrow)
-        if len(fortunatoDelete) > 0:
-            for rowinrow2 in fortunatoDelete:
-                if len(fortuna_teams) > 0:
-                    try:
-                        fortuna_teams.remove(rowinrow2)
-                    except ValueError:
-                        print("blad z meczem " + str(rowinrow2))
-        print(forbet_teams)
-        print(fortuna_teams)
-    print(i)
-    for row in leagues_data:
-        forbetid = row[0]
-        fortunaid = row[1]
-        forbettoDelete = []
-        fortunatoDelete = []
-        c.execute(
-            "SELECT t1, t2 FROM Fortuna_matches AS fm INNER JOIN Fortuna_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Fortuna_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(fortunaid)+"'")
-        fortuna_teams = c.fetchall()
-        print(len(fortuna_teams))
-        c.execute(
-            "SELECT t1, t2 FROM Forbet_matches AS fm INNER JOIN Forbet_match_odds AS fmo ON fmo.id = fm.id INNER JOIN Forbet_leagues AS fl ON fl.id = fmo.league_id  WHERE fl.id = '"+str(forbetid)+"'")
-        forbet_teams = c.fetchall()
-        for rowinforbet in forbet_teams:
-                for rowinfortuna in fortuna_teams:
-                    if rowinfortuna in listaostaczena:
-                        isFind == True
-                    else:
-                        #print (str(rowinforbet[0]) + " " + str(jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]))+" "+ str(rowinfortuna[0]) + " " + str(rowinforbet[1]) + " " + str(jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1])) + " " + str(rowinfortuna[1]))
-                        if jellyfish.jaro_winkler(rowinforbet[0], rowinfortuna[0]) > 0.4 and jellyfish.jaro_winkler(rowinforbet[1], rowinfortuna[1]) > 0.4:
-                            print(rowinforbet + rowinfortuna)
-                            i +=1
-                            forbettoDelete.append(rowinforbet)
-                            fortunatoDelete.append(rowinfortuna)
-                            listaostaczena.append(rowinforbet)
-                            listaostaczena.append(rowinfortuna)
-                            isFind = True
-                            break
-                if isFind == False:
-                    print(rowinforbet)
-                else:
-                    isFind = False
-        listaostaczena.append(forbettoDelete)
-        listaostaczena.append(fortunatoDelete)
-        if len(forbettoDelete) > 0:
-            for rowinrow in forbettoDelete:
-                forbet_teams.remove(rowinrow)
-        if len(fortunatoDelete) > 0:
-            for rowinrow2 in fortunatoDelete:
-                if len(fortuna_teams) > 0:
-                    try:
-                        fortuna_teams.remove(rowinrow2)
-                    except ValueError:
-                        print("blad z meczem " + str(rowinrow2))
-        print(forbet_teams)
-        print(fortuna_teams)
-    print(i)
-    y = 0
-    for x in range(len(listaostaczena)):
-        print(str(listaostaczena[y][0]) + " " + str(listaostaczena[y+1][0]))
-        print(str(listaostaczena[y][1]) + " " + str(listaostaczena[y + 1][1]))
-        y += 2
+
+def delete_matchedleagues_table():
+    c.execute("DROP TABLE IF EXISTS matched_leagues")
+
+
+def delete_matchedteams_table():
+    c.execute("DROP TABLE IF EXISTS matched_teams")
+
+
+def select_matches_from_league(bookie):
+    c.execute("SELECT t1, t2, date, league_id, site FROM " + bookie + "_matches AS fm INNER JOIN" + bookie +
+              "_leagues AS fl ON fm.league_id = fl.id")
+    data = c.fetchall()
+    return data
+
+
+def select_matches_from_league_with_date_and_team1():
+    c.execute("SELECT t1, t2, date, league_id, site FROM Forbet_matches AS fm INNER JOIN Forbet_leagues AS fl ON "
+              "fm.league_id = fl.id WHERE t1 = '" + match[0] + "' AND date = '"
+              + match[2] + "'")
--- matchteams.py
+++ matchteams.py
@@ -6,106 +6,93 @@ import database
 conn = sqlite3.connect('bazadanych.db')
 c = conn.cursor()
 
-# database.insert_all_teams()
-#
-# def create_matchedleagues_table():
-#     c.execute("DROP TABLE IF EXISTS matchedleagues_table")
-#     c.execute('CREATE TABLE IF NOT EXISTS matchedleagues_table(idFortuna INT, siteFortuna STRING, '
-#               'idForbet INT, siteForbet STRING, FOREIGN KEY (idFortuna) REFERENCES Fortuna_leagues(id), FOREIGN KEY '
-#               '(idForbet) REFERENCES Forbet_leagues(id))')
-#     conn.commit()
-#
-#
-# def create_matchedteams_table():
-#     c.execute("DROP TABLE IF EXISTS matchedteams")
-#     c.execute('CREATE TABLE IF NOT EXISTS matchedteams(id INTEGER PRIMARY KEY, idFortuna INT, idForbet INT, '
-#               'nameFortuna STRING, nameForbet STRING, FOREIGN KEY (idFortuna) REFERENCES Fortuna_teams(id), '
-#               'FOREIGN KEY (idForbet) REFERENCES Forbet_teams(id))')
-#     conn.commit()
-#
-# create_matchedleagues_table()
-# create_matchedteams_table()
-# def matchleagues():
-#     c.execute("SELECT t1, t2, date, league_id, site FROM Fortuna_matches AS fm INNER JOIN Fortuna_leagues AS fl ON "
-#               "fm.league_id = fl.id")
-#     fortuna_matches = c.fetchall()
-#     c.execute("SELECT t1, t2, date, league_id FROM Forbet_matches")
-#     forbet_matches = c.fetchall()
-#     for match in fortuna_matches:
-#         c.execute("SELECT t1, t2, date, league_id, site FROM Forbet_matches AS fm INNER JOIN Forbet_leagues AS fl ON "
-#                   "fm.league_id = fl.id WHERE t1 = '" + match[0] + "' AND date = '"
-#                   + match[2] + "'")
-#         data = c.fetchall()
-#         if len(data)>0:
-#             c.execute("SELECT idFortuna FROM matchedleagues_table WHERE idFortuna = " + str(match[3]))
-#             check = c.fetchall()
-#             if len(check) == 0:
-#                 c.execute("INSERT INTO matchedleagues_table VALUES (?, ?, ?, ?)",
-#                           (match[3], match[4], data[0][3], data[0][4]))
-#                 conn.commit()
-#             if len(data)>1:
-#                 print("okurwaco")
-#             print(data)
-#         c.execute("SELECT t1, t2, date, league_id, site FROM Forbet_matches AS fm INNER JOIN Forbet_leagues AS fl ON "
-#                   "fm.league_id = fl.id WHERE t1 = '" + match[1] + "' AND date = '"
-#                   + match[2] + "'")
-#         data = c.fetchall()
-#         if len(data) > 0:
-#             c.execute("SELECT idFortuna FROM matchedleagues_table WHERE idFortuna = " + str(match[3]))
-#             check = c.fetchall()
-#             if len(check) == 0:
-#                 c.execute("INSERT INTO matchedleagues_table VALUES (?, ?, ?, ?)",
-#                           (match[3], match[4], data[0][3], data[0][4]))
-#                 conn.commit()
-#             if len(data) > 1:
-#                 print("okurwaco")
-#             print(data)
-#
-#
-# matchleagues()
-#
-# def matchteams():
-#     c.execute("SELECT idFortuna, idForbet FROM matchedleagues_table")
-#     data = c.fetchall()
-#
-#     for league in data:
-#         c.execute("SELECT t1, t2, date FROM Fortuna_matches WHERE league_id = " + str(league[0]))
-#         fortuna_matches = c.fetchall()
-#         print(fortuna_matches)
-#         c.execute("SELECT t1, t2, date FROM Forbet_matches WHERE league_id = " + str(league[1]))
-#         forbet_matches = c.fetchall()
-#         print(forbet_matches)
-#         for fortunamatch in fortuna_matches:
-#             for forbetmatch in forbet_matches:
-#                 fortunatime = datetime.strptime(fortunamatch[2], '%Y-%m-%d %H:%M')
-#                 forbettime = datetime.strptime(forbetmatch[2], '%Y-%m-%d %H:%M')
-#
-#                 if fortunatime == forbettime:
-#                     fortunaone = fortunamatch[0].rstrip(".").replace(".", " ").split(" ")
-#                     fortunatwo = fortunamatch[1].rstrip(".").replace(".", " ").split(" ")
-#                     forbetone = forbetmatch[0].rstrip(".").replace(".", " ").split(" ")
-#                     forbettwo = forbetmatch[1].rstrip(".").replace(".", " ").split(" ")
-#                     findone = any([a == b for (a, b) in itertools.product(fortunaone, forbetone)]) == True
-#                     findtwo = any([a == b for (a, b) in itertools.product(fortunatwo, forbettwo)]) == True
-#
-#                     if findone or findtwo:
-#                         c.execute('SELECT id from Fortuna_teams WHERE Fortuna_name = "' + fortunamatch[0] + '"')
-#                         fortunaoneid = c.fetchall()
-#                         c.execute('SELECT id from Fortuna_teams WHERE Fortuna_name = "' + fortunamatch[1] + '"')
-#                         fortunatwoid = c.fetchall()
-#                         c.execute('SELECT id from Forbet_teams WHERE Forbet_name = "' + forbetmatch[0] + '"')
-#                         forbetoneid = c.fetchall()
-#                         c.execute('SELECT id from Forbet_teams WHERE Forbet_name = "' + forbetmatch[1] + '"')
-#                         forbettwoid = c.fetchall()
-#                         c.execute("INSERT INTO matchedteams VALUES (?, ?, ?, ?, ?)",
-#                                   (None, fortunaoneid[0][0], forbetoneid[0][0], fortunamatch[0], forbetmatch[0]))
-#                         conn.commit()
-#                         c.execute("INSERT INTO matchedteams VALUES (?, ?, ?, ?, ?)",
-#                                   (None, fortunatwoid[0][0], forbettwoid[0][0], fortunamatch[1], forbetmatch[1]))
-#                         conn.commit()
-#                         print(fortunamatch[0] + " " + fortunamatch[1] + " " + forbetmatch[0] + " " + forbetmatch[1])
+database.insert_all_teams()
 
-# matchteams()
+database.delete_matchedleagues_table()
+database.delete_matchedteams_table()
+database.create_matchedleagues_table()
+database.create_matchedteams_table()
+
+
+def matchleagues():
+    fortuna_matches = database.select_matches_from_league("Fortuna")
+    c.execute("SELECT t1, t2, date, league_id FROM Forbet_matches")
+    forbet_matches = c.fetchall()
+    for match in fortuna_matches:
+        c.execute("SELECT t1, t2, date, league_id, site FROM Forbet_matches AS fm INNER JOIN Forbet_leagues AS fl ON "
+                  "fm.league_id = fl.id WHERE t1 = '" + match[0] + "' AND date = '"
+                  + match[2] + "'")
+        data = c.fetchall()
+        if len(data)>0:
+            c.execute("SELECT idFortuna FROM matched_leagues WHERE idFortuna = " + str(match[3]))
+            check = c.fetchall()
+            if len(check) == 0:
+                c.execute("INSERT INTO matched_leagues VALUES (?, ?, ?, ?)",
+                          (match[3], match[4], data[0][3], data[0][4]))
+                conn.commit()
+            if len(data)>1:
+                print("okurwaco")
+            print(data)
+        c.execute("SELECT t1, t2, date, league_id, site FROM Forbet_matches AS fm INNER JOIN Forbet_leagues AS fl ON "
+                  "fm.league_id = fl.id WHERE t1 = '" + match[1] + "' AND date = '"
+                  + match[2] + "'")
+        data = c.fetchall()
+        if len(data) > 0:
+            c.execute("SELECT idFortuna FROM matched_leagues WHERE idFortuna = " + str(match[3]))
+            check = c.fetchall()
+            if len(check) == 0:
+                c.execute("INSERT INTO matched_leagues VALUES (?, ?, ?, ?)",
+                          (match[3], match[4], data[0][3], data[0][4]))
+                conn.commit()
+            if len(data) > 1:
+                print("okurwaco")
+            print(data)
+
+
+matchleagues()
+
+def matchteams():
+    c.execute("SELECT idFortuna, idForbet FROM matched_leagues")
+    data = c.fetchall()
+
+    for league in data:
+        c.execute("SELECT t1, t2, date FROM Fortuna_matches WHERE league_id = " + str(league[0]))
+        fortuna_matches = c.fetchall()
+        print(fortuna_matches)
+        c.execute("SELECT t1, t2, date FROM Forbet_matches WHERE league_id = " + str(league[1]))
+        forbet_matches = c.fetchall()
+        print(forbet_matches)
+        for fortunamatch in fortuna_matches:
+            for forbetmatch in forbet_matches:
+                fortunatime = datetime.strptime(fortunamatch[2], '%Y-%m-%d %H:%M')
+                forbettime = datetime.strptime(forbetmatch[2], '%Y-%m-%d %H:%M')
+
+                if fortunatime == forbettime:
+                    fortunaone = fortunamatch[0].rstrip(".").replace(".", " ").split(" ")
+                    fortunatwo = fortunamatch[1].rstrip(".").replace(".", " ").split(" ")
+                    forbetone = forbetmatch[0].rstrip(".").replace(".", " ").split(" ")
+                    forbettwo = forbetmatch[1].rstrip(".").replace(".", " ").split(" ")
+                    findone = any([a == b for (a, b) in itertools.product(fortunaone, forbetone)]) == True
+                    findtwo = any([a == b for (a, b) in itertools.product(fortunatwo, forbettwo)]) == True
+
+                    if findone or findtwo:
+                        c.execute('SELECT id from Fortuna_teams WHERE Fortuna_name = "' + fortunamatch[0] + '"')
+                        fortunaoneid = c.fetchall()
+                        c.execute('SELECT id from Fortuna_teams WHERE Fortuna_name = "' + fortunamatch[1] + '"')
+                        fortunatwoid = c.fetchall()
+                        c.execute('SELECT id from Forbet_teams WHERE Forbet_name = "' + forbetmatch[0] + '"')
+                        forbetoneid = c.fetchall()
+                        c.execute('SELECT id from Forbet_teams WHERE Forbet_name = "' + forbetmatch[1] + '"')
+                        forbettwoid = c.fetchall()
+                        c.execute("INSERT INTO matched_teams VALUES (?, ?, ?, ?, ?)",
+                                  (None, fortunaoneid[0][0], forbetoneid[0][0], fortunamatch[0], forbetmatch[0]))
+                        conn.commit()
+                        c.execute("INSERT INTO matched_teams VALUES (?, ?, ?, ?, ?)",
+                                  (None, fortunatwoid[0][0], forbettwoid[0][0], fortunamatch[1], forbetmatch[1]))
+                        conn.commit()
+                        print(fortunamatch[0] + " " + fortunamatch[1] + " " + forbetmatch[0] + " " + forbetmatch[1])
+
+matchteams()
 
 def matchmatches():
     c.execute("DROP TABLE IF EXISTS matches")
@@ -116,13 +103,13 @@ def matchmatches():
     matches = c.fetchall()
     for match in matches:
         print(match)
-        c.execute('SELECT nameForbet FROM matchedteams WHERE nameFortuna = "' + str(match[1]) + '"')
+        c.execute('SELECT nameForbet FROM matched_teams WHERE nameFortuna = "' + str(match[1]) + '"')
         forbetnameone = c.fetchall()
         if len(forbetnameone):
             forbetnameone = forbetnameone[0][0]
         else:
             forbetnameone = ''
-        c.execute('SELECT nameForbet FROM matchedteams WHERE nameFortuna = "' + str(match[2]) + '"')
+        c.execute('SELECT nameForbet FROM matched_teams WHERE nameFortuna = "' + str(match[2]) + '"')
         forbetnametwo = c.fetchall()
         if len(forbetnametwo):
             forbetnametwo = forbetnametwo[0][0]
